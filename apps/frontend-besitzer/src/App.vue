f63814a248dd92131f983f232228251265d58c2d
Kay <info@beckmann-md.de>
2025-05-03 20:21:47 +0200

    Frontend issues

--- apps/frontend-besitzer/src/App.vue
+++ apps/frontend-besitzer/src/App.vue
@@ -2,16 +2,23 @@
 </script>
 
 <template>
-  <h1>Besitzer</h1> <nav>
+  <div class="content">
+    <nav>
+      <h1>Besitzer</h1> 
     </nav>
-  <main>
-    <router-view />
-  </main>
-  <footer>
-  <h1>Footer</h1>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <h1>Footer</h1>
     </footer>
+  </div>
 </template>
 
-<style>
-/* Globale Styles oder Styles für das App-Layout */
+<style scoped>
+.content{
+  background-color: purple;
+  height: 100dvh;
+  display: grid;
+}
 </style>
