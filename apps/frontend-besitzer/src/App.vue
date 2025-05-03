efa61b442ed089bc64aecf2c0150405c2518f0cc
Kay <info@beckmann-md.de>
2025-05-03 20:47:52 +0200

    try some styling

--- apps/frontend-besitzer/src/App.vue
+++ apps/frontend-besitzer/src/App.vue
@@ -3,11 +3,15 @@
 
 <template>
   <div class="content">
+    <header>
+      <h1>Header</h1> 
+    </header>
     <nav>
-      <h1>Besitzer</h1> 
+      <h1>Navigation</h1>
     </nav>
-    <main>
-      <router-view />
+    <main class="content">
+      <!-- <router-view /> -->
+       <h1>content</h1>
     </main>
     <footer>
       <h1>Footer</h1>
@@ -17,8 +21,34 @@
 
 <style scoped>
 .content{
-  background-color: purple;
   height: 100dvh;
   display: grid;
+  grid-template-areas:
+  "nav head head"
+  "nav content content"
+  "footer footer footer";
+  grid-template-rows: 50px 1fr 30px;
+  grid-template-columns: 150px 1fr;
+}
+
+header {
+  grid-area: head;
+  background-color: #8ca0ff;
+}
+
+nav {
+  grid-area: nav;
+  background-color: lightgreen;
+}
+
+.content{
+  grid-area: content;
+  background-color: yellow;
+}
+
+footer{
+  grid-area: footer;
+  background-color: black;
+  color: white;
 }
 </style>
