233b2597f56256f86d164c091a3a7d2370cf4cbd
Kay <info@beckmann-md.de>
2025-05-04 11:20:43 +0200

    Edit Layout

--- apps/frontend-besitzer/src/App.vue
+++ apps/frontend-besitzer/src/App.vue
@@ -1,54 +1,69 @@
 <script setup lang="ts">
+import NavigationComponent from './components/NavigationComponent.vue';
+
 </script>
 
 <template>
-  <div class="content">
+  <div class="container">
     <header>
       <h1>Header</h1> 
     </header>
     <nav>
-      <h1>Navigation</h1>
+      <NavigationComponent />
     </nav>
     <main class="content">
-      <!-- <router-view /> -->
-       <h1>content</h1>
+      <router-view />
     </main>
     <footer>
-      <h1>Footer</h1>
+      <div>Footer</div>
     </footer>
   </div>
 </template>
 
 <style scoped>
-.content{
+.container{
   height: 100dvh;
   display: grid;
   grid-template-areas:
   "nav head head"
   "nav content content"
   "footer footer footer";
-  grid-template-rows: 50px 1fr 30px;
-  grid-template-columns: 150px 1fr;
+  grid-template-rows: var(--hight-header) 1fr var(--hight-footer);
+  grid-template-columns: var(--width-nav) 1fr;
 }
 
 header {
   grid-area: head;
-  background-color: #8ca0ff;
+  background-color: var(--color-primary);
 }
 
 nav {
   grid-area: nav;
-  background-color: lightgreen;
+  background-color: var(--color-secondary);
 }
 
 .content{
   grid-area: content;
-  background-color: yellow;
+  background-color: var(--color-background);
 }
 
 footer{
   grid-area: footer;
   background-color: black;
-  color: white;
+  color: var(--color-background);
+}
+</style>
+
+<style>
+:root {
+  --color-primary: #2f4de4;
+  --color-secondary: #8ca0ff;
+  --color-accent: #ff4081;
+  --color-background: #e7e5e5;
+  --color-text: #333333;
+
+  --hight-header: 80px;
+  --hight-footer: 60px;
+  --width-nav: 200px;
 }
 </style>
