3a189806b794c15cbc26f8d417138082e82f6d2d
Kay <info@beckmann-md.de>
2025-05-04 11:28:11 +0200

    add some static components

--- apps/frontend-besitzer/src/views/DashboardView.vue
+++ apps/frontend-besitzer/src/views/DashboardView.vue
@@ -1,8 +1,43 @@
 <template>
-    <h1>Dashboard</h1>
+    <h1>Meine Tiere</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Tier</th>
+                <th>Rasse</th>
+                <th>Alter</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="tier in tiere" :key="tier.id">
+                <td>{{ tier.name }}</td>
+                <td>{{ tier.rasse }}</td>
+                <td>{{ tier.alter }}</td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="tasks">
+        <h2>Tägliche Aufgaben:</h2>
+        <ul>
+            <li>Füttern</li>
+            <li>Gassi gehen</li>
+            <li>Tierarztbesuch</li>
+            <li>Pflegen</li>
+            <li>Spielen</li>
+            <li>Sauber machen</li>
+        </ul>
+    </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { defineComponent, ref } from 'vue';
+
+let tiere = ref([
+    { id: 1, name: 'Bello', rasse: 'Labrador', alter: 5 },
+    { id: 2, name: 'Miau', rasse: 'Siamkatze', alter: 3 },
+    { id: 3, name: 'Fido', rasse: 'Beagle', alter: 4 },
+]);
+
 </script>
 
 <style scoped>
